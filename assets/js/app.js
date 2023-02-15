f9a3bb40c048ab5035d056f6df0c22b2f7b84eef
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-02-15 14:48:30 +0400

    first comit

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText =
+    hour + " : " + min + " : " + sec; /* adding time to the div */
+  var t = setTimeout(() => {
+    currentTime();
+  }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
+
+currentTime();
+//timeline portfi
+
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if (!items[i].classList.contains("in-view")) {
+        items[i].classList.add("in-view");
+      }
+    } else if (items[i].classList.contains("in-view")) {
+      items[i].classList.remove("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
