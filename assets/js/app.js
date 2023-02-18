943fef437a9daec30fbc046ff6a1660ec2941890
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-02-18 12:46:18 +0400

    last comit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 function currentTime() {
-  var date = new Date(); /* creating object of Date class */
+  var date = new Date(); 
   var hour = date.getHours();
   var min = date.getMinutes();
   var sec = date.getSeconds();
@@ -7,12 +7,12 @@ function currentTime() {
   min = updateTime(min);
   sec = updateTime(sec);
   document.getElementById("clock").innerText =
-    hour + " : " + min + " : " + sec; /* adding time to the div */
+    hour + " : " + min + " : " + sec; 
   var t = setTimeout(() => {
     currentTime();
   }, 1000); /* setting timer */
 }
-
+//saatin qabagina 0 elave elemek ucun
 function updateTime(k) {
   if (k < 10) {
     return "0" + k;
@@ -52,3 +52,15 @@ function callbackFunc() {
 
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
+
+//overlay menu yaradilmasi
+
+const myBurgerIcon= document.getElementById("burger-icon")
+const overlay = document.getElementById("overlay-menu")
+const overlayExit= document.getElementById("overlay-exit")
+myBurgerIcon.onclick = function(){
+  overlay.style.display= 'block'
+}
+overlayExit.onclick= function(){
+  overlay.style.display= 'none'
+}
